5049b66ec8f30972345ea09f6ecbc574b278db0b
Nathan Lewis <nathanblaiselewis@gmail.com>
2017-09-20 22:28:32 -0400

    Cell classes changes and updates made.

--- src/cellsociety_team02/cells/PredPreyCell.java
+++
@@ -1,9 +0,0 @@
-package cellsociety_team02.cells;
-
-import javafx.scene.paint.Color;
-
-public class PredPreyCell extends Cell {
-	public PredPreyCell(int xPos, int yPos, int startingState, Color[] colors, int sideLength) {
-		super(xPos, yPos, startingState, colors, sideLength);
-	}
-}
--- src/cellsociety_team02/cells/SegregationCell.java
+++
@@ -1,9 +0,0 @@
-package cellsociety_team02.cells;
-
-import javafx.scene.paint.Color;
-
-public class SegregationCell extends Cell {
-	public SegregationCell(int xPos, int yPos, int startingState, Color[] colors, int sideLength) {
-		super(xPos, yPos, startingState, colors, sideLength);
-	}
-}
