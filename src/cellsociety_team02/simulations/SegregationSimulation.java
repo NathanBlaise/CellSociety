fcf87c08ef738ad7b067069fc3d9432abf26e199
Nathan Lewis <nathanblaiselewis@gmail.com>
2017-09-23 16:02:11 -0400

    Quick fixes made

--- src/cellsociety_team02/simulations/SegregationSimulation.java
+++ src/cellsociety_team02/simulations/SegregationSimulation.java
@@ -1,15 +1,11 @@
 package cellsociety_team02.simulations;
 
 import java.util.List;
-import java.util.Queue;
 
 import cellsociety_team02.cells.Cell;
 
 public class SegregationSimulation extends Simulation{
 	
-	private final int EMPTY = 0;
-	private final int GROUP1 = 1;
-	private final int GROUP2 = 2;
 	private final double PERCENTAGE_AGENT = 0.3;
 	
 	private int total;
