10e7d26158fb4fec8eb7cb480131fd5015ba6038
Nathan Lewis <nathanblaiselewis@gmail.com>
2017-09-22 21:43:27 -0400

    GoL Sim done, others set up

--- src/cellsociety_team02/simulations/LifeSimulation.java
+++ src/cellsociety_team02/simulations/LifeSimulation.java
@@ -1,5 +1,35 @@
 package cellsociety_team02.simulations;
 
-public class LifeSimulation extends Simulation {
+import java.util.List;
 
+import cellsociety_team02.cells.Cell;
+
+public class LifeSimulation extends Simulation {
+	
+	private final int DEAD = 0;
+	private final int ALIVE = 1;
+	
+	@Override
+	public void updateCell(Cell cell) {
+		cell.setNextState(DEAD);
+		calculateState(cell);
+	}
+	
+	private void calculateState(Cell cell) {
+		int total = getNumNeighbours(cell);
+		if(cell.getCurrentState() == ALIVE) {
+			if(total == 2) cell.setNextState(ALIVE);
+		}
+		if(total == 3) cell.setNextState(ALIVE);
+	}
+
+	private int getNumNeighbours(Cell cell) {
+		List<Cell> neighbours = cell.getNeighbours();
+		int total = 0;
+		for(Cell neighbour:neighbours) {
+			total += neighbour.getCurrentState();
+		}
+		return total;
+	}
+	
 }
